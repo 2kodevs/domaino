105175ae40b3358ebb0a96d81e5bde27394db48a
Lazaro Raul <riglesiasvera@gmail.com>
2020-10-27 00:18:30 -0400

    Implamentation of FirstToGain100 game rules

--- rules.py
+++ rules.py
@@ -59,7 +59,45 @@ class TwoOfThree:
 
         return 0 if wins[0] > wins[1] else 1
 
+
+class FirstToGain100:
+    """
+        First to team that gain 100 points. Last winner start next match
+    """
+    def __init__(self, random_start=True):
+        self.random_start = random_start
+
+    def start(self, player0, player1, *pieces_config):
+        env = DominoManager()
+        players = [player0("0"), player1("1"), player0("2"), player1("3")]
+
+        cur_start = 0
+
+        if self.random_start:
+            if random.choice([False, True]):
+                cur_start ^= 1
+                players[0], players[1] = players[1], players[0]
+                players[2], players[3] = players[3], players[2]
+
+        points = [0, 0]
+
+        while max(points) < 100:
+            result = env.run(players, *pieces_config)
+
+            if result != -1:
+                loser = result ^ 1
+                points[result] += players[loser].score() + players[loser + 2].score()
+
+            if result == -1 or result != cur_start:
+                # Swap players
+                cur_start ^= 1
+                players[0], players[1] = players[1], players[0]
+                players[2], players[3] = players[3], players[2]
+
+        return 0 if points[0] > points[1] else 1
+
 RULES = [
     OneGame,
     TwoOfThree,
+    FirstToGain100,
 ]
