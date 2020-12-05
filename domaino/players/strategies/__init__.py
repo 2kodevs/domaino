f965d96ed9304ab4d4647517cc9d0d61c84828cc
Lazaro Raul <riglesiasvera@gmail.com>
2020-12-05 18:10:03 -0500

    Add SmallDrop player

--- domaino/players/strategies/__init__.py
+++ domaino/players/strategies/__init__.py
@@ -6,6 +6,7 @@ from .supportive import Supportive
 from .player_merge import MergeFactory
 from .simpleh import SimpleHybrid
 from .mc import MonteCarlo
+from .small_drop import SmallDrop
 
 # Add players to this list
 PLAYERS = [
@@ -18,6 +19,7 @@ PLAYERS = [
     TableCounter,
     Passer,
     Supportive,
+    SmallDrop,
 ]
 
 def get_player(value, merge=True):
