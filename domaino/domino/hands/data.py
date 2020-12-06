3c2132292d9064dccb6d5e76336cd8ce53ac95a8
Carmen I Cabrera <kr1000a@gmail.com>
2020-12-06 11:33:37 -0500

    Fix amount of pieces to choose from the same number

--- domaino/domino/hands/data.py
+++ domaino/domino/hands/data.py
@@ -3,7 +3,7 @@ from random import sample, choice
 
 def data(max_number, pieces_per_player, high=True):
     """
-    Force player 0 to have 5 or more pieces of the same number.
+    Force player0 to have at least half of his pieces of the same number.
     Randomly distribute pieces among the other players.
     Valid pieces are all integer tuples of the form:
         (i, j) 0 <= i <= j <= max_number
@@ -12,7 +12,8 @@ def data(max_number, pieces_per_player, high=True):
 
     mid = (max_number + 1) // 2 + 1
     data_number = choice(list(range([0, mid][high], [mid, max_number + 1][high])))
-    hand = [(min(data_number, i), max(data_number, i)) for i in sample(list(range(max_number + 1)), 5)]
+    cant = min(max_number + 1, pieces_per_player) // 2
+    hand = [(min(data_number, i), max(data_number, i)) for i in sample(list(range(max_number + 1)), cant)]
 
     pieces = [(i, j) for i in range(max_number + 1) for j in range(max_number + 1) if i <= j and (i, j) not in hand]
     assert 4 * pieces_per_player <= len(pieces) + len(hand)
