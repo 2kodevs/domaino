53bf5535f4227d9b5fedd376a3fd7b6d7ee3cce0
AntiD2ta <mtpotro41@gmail.com>
2020-12-06 01:09:45 -0500

    Restructure domaino submodule to support several hands

--- domaino/domino/__init__.py
+++ domaino/domino/__init__.py
@@ -1,1 +1,13 @@
 from .domino import *
+from .utils import *
+
+HANDS = [
+    hand_out,
+]
+
+def get_hand(value):
+    value = value.lower()
+    for obj in HANDS:
+        if obj.__name__.lower() == value:
+            return obj
+    raise ValueError(f"{value} not found in {[r.__name__ for r in HANDS]}")
