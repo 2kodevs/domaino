e8567b54ca3023e91efd8d4499e96ed6fe6022ab
AntiD2ta <mtpotro41@gmail.com>
2020-12-06 12:17:21 -0500

    Amend some errors in share_data

--- domaino/domino/hands/utils.py
+++ domaino/domino/hands/utils.py
@@ -7,13 +7,15 @@ def share_data(max_number, pieces_per_player, high):
     all_pieces = {(i, j) for i in range(max_number + 1) for j in range(max_number + 1) if i <= j}
 
     data0 = data[:randint(1, len(data))]
-    pieces0 = all_pieces - set(data0)
+    pieces0 = all_pieces - set(data)
     hand0 = sample(pieces0, pieces_per_player - len(data0))
+    pieces0 -= hand0
     hand0 += data0
 
     data1 = set(data) - set(data0)
     pieces1 = pieces0 - set(data1)
     hand1 = sample(pieces1, pieces_per_player - len(data1))
+    pieces1 -= hand1
     hand1 += data1
 
     assert 4 * pieces_per_player <= len(pieces1) + len(hand1) + len(hand0)
