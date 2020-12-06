a7d13f8c6e98c984145b993131ab09082148ee66
AntiD2ta <mtpotro41@gmail.com>
2020-12-05 23:42:32 -0500

    Implement Best Behavior
    Play higher better accompanied if possible as the first move of the game

---
+++ domaino/players/behaviors/best.py
@@ -0,0 +1,38 @@
+from ..player import BasePlayer
+from random import choice
+
+class Best(BasePlayer):
+    ''' Play higher better accompanied if posible as the first move of the game
+    '''
+    def __init__(self, name):
+        super().__init__(f'Best::{name}')
+
+    def filter(self, valids=None):
+        valids = super().filter(valids)
+
+        if self.heads != [-1, -1]:
+            return valids
+        
+        cant = {}
+        for p0, p1 in self.pieces:
+            if p0 != p1:
+                cant[p1] = cant.get(p1, 0) + 1
+            cant[p0] = cant.get(p0, 0) + 1
+
+        filtered = [(c, num) for num, c in cant.items() if num >= 6 and c >= 2]
+        filtered.sort(reverse=True)
+
+        if not filtered:
+            return valids
+        
+        best = filtered[0][0]
+        filtered = [(num, c) for c, num in filtered if c == best]
+        filtered.sort(reverse=True)
+
+        best = filtered[0][0]
+        pieces = [p for p in self.pieces if best in p]
+
+        if (best, best) in pieces:
+            return (best, best)
+        return choice(pieces)
+    
