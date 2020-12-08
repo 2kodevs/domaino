b0c954b4cb18a93d6d5983e44317c207bae1b770
AntiD2ta <mtpotro41@gmail.com>
2020-12-08 18:57:00 -0500

    Register DoubleEnd player

--- domaino/players/strategies/__init__.py
+++ domaino/players/strategies/__init__.py
@@ -12,6 +12,7 @@ from .small_drop import SmallDrop
 from .agachao import Agachao
 from .data_dropper import DataDropper
 from .always_double import AlwaysDouble
+from .double_end import DoubleEnd
 
 # Add players to this list
 PLAYERS = [
@@ -29,5 +30,6 @@ PLAYERS = [
     SmallDrop,
     Agachao,
     DataDropper,
-    AlwaysDouble
+    AlwaysDouble,
+    DoubleEnd,
 ]
