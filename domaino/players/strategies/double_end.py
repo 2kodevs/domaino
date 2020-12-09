bfe4352ef330c15bc9e39cd7a7592c6cc1e1d290
Miguel Tenorio <46824157+stdevAntiD2ta@users.noreply.github.com>
2020-12-08 22:11:01 -0500

    Fix name attr of DoubleEnd player
    Co-authored-by: Lázaro Raúl <47638426+stdevRulo@users.noreply.github.com>

--- domaino/players/strategies/double_end.py
+++ domaino/players/strategies/double_end.py
@@ -5,7 +5,7 @@ class DoubleEnd(BasePlayer):
     This player always selects a double piece if possible to make a move at the end of the game.
     '''
     def __init__(self, name):
-         super().__init__(f"AlwaysDouble::{name}")
+         super().__init__(f"DoubleEnd::{name}")
 
     def filter(self, valids=None):
         valids = super().filter(valids)
